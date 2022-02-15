a814425d04062c4c444b16453cc9dd3f733fb488
Mikkel Ofrim <39702459+Mikkelof@users.noreply.github.com>
2022-02-15 13:06:24 +0100

    Removed vee-validate import from main.js

--- oving2-3/src/main.js
+++ oving2-3/src/main.js
@@ -1,7 +1,6 @@
 import { createApp } from "vue";
-import VeeValidate from "vee-validate";
 import App from "./App.vue";
 import store from './store/store'
 import router from './router'
 
-createApp(App).use(router).use(store).use(VeeValidate).mount("#app");
+createApp(App).use(router).use(store).mount("#app");
