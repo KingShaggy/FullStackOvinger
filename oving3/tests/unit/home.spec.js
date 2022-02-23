9f01ec47edf067b5a3fec57de089ec01dfe67271
Mikkel Ofrim <39702459+Mikkelof@users.noreply.github.com>
2022-02-23 10:43:31 +0100

    Update home.spec.js

--- oving3/tests/unit/home.spec.js
+++ oving3/tests/unit/home.spec.js
@@ -1,54 +1,46 @@
 import Home from '@/views/Home.vue'
 import { mount } from '@vue/test-utils'
-import { createStore } from "vuex"
+import { createStore } from '@/store'
 import router from '@/router'
 
-const store = createStore({
-    state: {
-      username: '',
-      password: '',
-      fullName: '',
-      token: null,
-    },
-    mutations: {
-      updateUsername(state, payload) {
-        state.usern = payload;
-      },
-      updatePassword(state, payload) {
-        state.password = payload;
-      },
-      updateFullName(state, payload) {
-        state.fullName = payload;
-      },
-      updateToken(state, payload) {
-        state.token = payload;
-      }
-    }
-})
+function mountHome(config = {}) {
+    config.mountOptions = config.mountOptions || {}
+    config.plugins = config.plugins || {}
+
+    return mount(Home, {
+        global: {
+            plugins: [createStore(config.plugins.store), router]
+        },
+        ...config.mountOptions
+    })
+}
 
 describe('Home', () => {
     test('Home is displayed when user is authenticated', async () => {
-        const wrapper = mount(Home, {
-            global: {
-                plugins: [store, router]
-            }
+        const wrapper = mountHome({
+            plugins: [{
+                store: {
+                    state: () => ({
+                        token: 1,
+                    })
+                },
+            }, router]
         })
-        store.commit("updateToken", 123);
-
-        router.push('/')
         await router.isReady()
-
         expect(wrapper.html()).toContain('Hello')
     }),
     test('Login is displayed when user is not authenticated', async () => {
-        const wrapper = mount(Home, {
-            global: {
-                plugins: [store, router]
-            }
-        })
-
         router.push('/')
         await router.isReady()
-        expect(window.location.href).toContain('login')
+        const wrapper = mountHome({
+            plugins: [{
+                store: {
+                    state: () => ({
+                        token: null,
+                    })
+                },
+            }, router]
+        })
+        expect(window.location.href).toEqual("http://localhost/login")
     })
 })
